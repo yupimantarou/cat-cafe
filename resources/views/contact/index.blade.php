82bee261a6509b663df9f3fe5996b14d003f81d7
yupiman <yukilv519@gmail.com>
2024-09-08 13:33:18 +0900

    入力内容の再取得機能の追加

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -32,35 +32,35 @@
         @csrf
         <div class="mb-4">
           <label for="name" class="block text-left p-1 my-1 font-medium">お名前<span class="text-white text-xs bg-yellow-400 mx-2 py-1 px-2">必須</span></label>
-          <input id="name" class="w-full p-4 text-xs leading-none bg-blueGray-50 rounded outline-none border" type="text" placeholder="例）田中太郎" name="name">
+          <input id="name" class="w-full p-4 text-xs leading-none bg-blueGray-50 rounded outline-none border" type="text" placeholder="例）田中太郎" name="name" value="{{ old("name") }}">
           @if($errors->has("name"))
           <p class="text-red-400">{{ $errors->first("name") }}</p>
           @endif
         </div>
         <div class="mb-4">
           <label for=name_kana class="block text-left p-1 my-1 font-medium">お名前（フリガナ）<span class="text-white text-xs bg-yellow-400 mx-2 py-1 px-2">必須</span></label>
-          <input id="name_kana" class="w-full p-4 text-xs leading-none bg-blueGray-50 rounded outline-none border" type="text" placeholder="例）タナカタロウ", name="name_kana">
+          <input id="name_kana" class="w-full p-4 text-xs leading-none bg-blueGray-50 rounded outline-none border" type="text" placeholder="例）タナカタロウ", name="name_kana" value="{{ old("name_kana") }}">
           @if($errors->has("name_kana"))
           <p class="text-red-400">{{ $errors->first("name_kana") }}</p>
           @endif
         </div>
         <div class="mb-4">
           <label for="phone" class="block text-left p-1 my-1 font-medium">電話番号</label>
-          <input id="phone" class="w-full p-4 text-xs leading-none bg-blueGray-50 rounded outline-none border" type="text" placeholder="例）0312345678" name="phone">
+          <input id="phone" class="w-full p-4 text-xs leading-none bg-blueGray-50 rounded outline-none border" type="text" placeholder="例）0312345678" name="phone" value="{{ old("phone") }}">
           @if($errors->has("phone"))
           <p class="text-red-400">{{ $errors->first("phone") }}</p>
           @endif
         </div>
         <div class="mb-4">
           <label for="email" class="block text-left p-1 my-1 font-medium">メールアドレス<span class="text-white text-xs bg-yellow-400 mx-2 py-1 px-2">必須</span></label>
-          <input id="email" class="w-full p-4 text-xs leading-none bg-blueGray-50 rounded outline-none border" type="email" placeholder="info@example.com" name="email">
+          <input id="email" class="w-full p-4 text-xs leading-none bg-blueGray-50 rounded outline-none border" type="email" placeholder="info@example.com" name="email" value="{{ old("email") }}">
           @if($errors->has("email"))
           <p class="text-red-400">{{ $errors->first("email") }}</p>
           @endif
         </div>
         <div class="mb-4">
           <label for="body" class="block text-left p-1 my-1 font-medium">お問い合わせ内容<span class="text-white text-xs bg-yellow-400 mx-2 py-1 px-2">必須</span></label>
-          <textarea id="body" class="w-full h-24 p-4 text-xs leading-none resize-none bg-blueGray-50 rounded outline-none border" type="text" placeholder="ご自由にご記入ください" name="body"></textarea>
+          <textarea id="body" class="w-full h-24 p-4 text-xs leading-none resize-none bg-blueGray-50 rounded outline-none border" type="text" placeholder="ご自由にご記入ください" name="body">{{ old("body") }}</textarea>
           @if($errors->has("body"))
           <p class="text-red-400">{{ $errors->first("body") }}</p>
           @endif
